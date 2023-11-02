ef418d3648ff108ee0181e52130e93f5135f864c
Svetoslav P. Vulkov <svetoslav.vulkov@gmail.com>
2023-11-02 12:06:35 +0200

    Basic prototyping

--- internal/RSSReader.go
+++ internal/RSSReader.go
@@ -1,7 +1,8 @@
 package RSSReader
 
 import (
-	"fmt"
+	"net/url"
+	"sync"
 	"time"
 )
 
@@ -14,10 +15,50 @@ type RSSItem struct {
 	Description string
 }
 
-func Parse(links []string) []RSSItem {
-	fmt.Println("Parsing")
+// add a context so I can cancel mid way?
+func parseRSSLink(link *url.URL, ch chan<- RSSItem, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var item RSSItem
 
-	return nil
+	// TODO: do some parsing and stuff
+
+	// pass the parsed item to the channel if valid, otherwise just be done with this go routine
+	ch <- item
+
+}
+
+func Parse(links []string) []RSSItem {
+	if links == nil {
+		return nil
+	}
+
+	waitGroup := sync.WaitGroup{}
+	rssChannel := make(chan RSSItem)
+
+	for _, link := range links {
+		// check if link is a valid link
+		currentLink, err := url.ParseRequestURI(link)
+		if err != nil {
+			panic(err)
+		}
+		// link is valid, add to a list of actual links?
+		// go parseRSSLink(currentLink) + some sort of channel?
+		waitGroup.Add(1)
+		go parseRSSLink(currentLink, rssChannel, &waitGroup)
+	}
+
+	go func() { // TODO:  really hacky, get rid of this later
+		waitGroup.Wait()
+		close(rssChannel)
+	}()
+
+	var parsedRSSItems []RSSItem
+
+	for item := range rssChannel {
+		parsedRSSItems = append(parsedRSSItems, item)
+	}
+
+	return parsedRSSItems
 }
 
 /*
